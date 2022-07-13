da3e4c87b9f9fa0da11e07ef8e3e8f218c9d7dcb
xWxfFle <xWxfFle@users.noreply.github.com>
2022-07-13 17:48:17 +0300

    master

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ class App {
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-var myGamePiece;
-var myObstacles = [];
-var myScore;
+let myGamePiece;
+const myObstacles = [];
+let myScore;
 
 function startGame() {
   myGameArea.start();
@@ -42,7 +42,7 @@ function startGame() {
   myScore = new Component("30px", "Consolas", "black", 280, 40, "text");
 }
 
-var myGameArea = {
+const myGameArea = {
   canvas: document.createElement("canvas"),
   start: function () {
     this.canvas.width = 1000;
@@ -88,22 +88,22 @@ class Component {
     this.hitBottom();
   }
   hitBottom() {
-    var rockbottom = myGameArea.canvas.height - this.height;
+    let rockbottom = myGameArea.canvas.height - this.height;
     if (this.y > rockbottom) {
       this.y = rockbottom;
       this.gravitySpeed = 0;
     }
   }
   crashWith(otherobj) {
-    var myleft = this.x;
-    var myright = this.x + this.width;
-    var mytop = this.y;
-    var mybottom = this.y + this.height;
-    var otherleft = otherobj.x;
-    var otherright = otherobj.x + otherobj.width;
-    var othertop = otherobj.y;
-    var otherbottom = otherobj.y + otherobj.height;
-    var crash = true;
+    let myleft = this.x;
+    let myright = this.x + this.width;
+    let mytop = this.y;
+    let mybottom = this.y + this.height;
+    let otherleft = otherobj.x;
+    let otherright = otherobj.x + otherobj.width;
+    let othertop = otherobj.y;
+    let otherbottom = otherobj.y + otherobj.height;
+    let crash = true;
     if (
       mybottom < othertop ||
       mytop > otherbottom ||
@@ -116,65 +116,8 @@ class Component {
   }
 }
 
-function component(width, height, color, x, y, type) {
-  this.type = type;
-  this.score = 0;
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-  this.gravity = 0;
-  this.gravitySpeed = 0;
-  this.update = function () {
-    ctx = myGameArea.context;
-    if (this.type == "text") {
-      ctx.font = this.width + " " + this.height;
-      ctx.fillStyle = color;
-      ctx.fillText(this.text, this.x, this.y);
-    } else {
-      ctx.fillStyle = color;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-  };
-  this.newPos = function () {
-    this.gravitySpeed += this.gravity;
-    this.x += this.speedX;
-    this.y += this.speedY + this.gravitySpeed;
-    this.hitBottom();
-  };
-  this.hitBottom = function () {
-    var rockbottom = myGameArea.canvas.height - this.height;
-    if (this.y > rockbottom) {
-      this.y = rockbottom;
-      this.gravitySpeed = 0;
-    }
-  };
-  this.crashWith = function (otherobj) {
-    var myleft = this.x;
-    var myright = this.x + this.width;
-    var mytop = this.y;
-    var mybottom = this.y + this.height;
-    var otherleft = otherobj.x;
-    var otherright = otherobj.x + otherobj.width;
-    var othertop = otherobj.y;
-    var otherbottom = otherobj.y + otherobj.height;
-    var crash = true;
-    if (
-      mybottom < othertop ||
-      mytop > otherbottom ||
-      myright < otherleft ||
-      myleft > otherright
-    ) {
-      crash = false;
-    }
-    return crash;
-  };
-}
-
 function updateGameArea() {
-  var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+  let x, height, gap, minHeight, maxHeight, minGap, maxGap;
   for (i = 0; i < myObstacles.length; i += 1) {
     if (myGamePiece.crashWith(myObstacles[i])) {
       return;
