740b76162e5d8ae4c1320410d8b0baed46468665
xWxfFle <factores175@gmail.com>
2022-08-09 19:40:38 +0300

    Updated gulp file, added scss support

--- gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-import browserSync from "browser-sync";
-export const server = (done) => {
-  browserSync.init({
-    server: {
-      baseDir: `${app.path.build.html}`,
-    },
-    port: 3000,
-  });
-};
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,53 @@
 import gulp from "gulp";
-import { path } from "./gulp/config/path.js";
-import { copy } from "./gulp/tasks/copy.js";
-import { html } from "./gulp/tasks/html.js";
-import { reset } from "./gulp/tasks/reset.js";
-import { server } from "./gulp/tasks/server.js";
+import browserSync from "browser-sync";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import { deleteAsync } from "del";
 
-global.app = {
-  path: path,
-  gulp: gulp,
+const reset = () => {
+  return deleteAsync("dist");
 };
+
+const copy = () => {
+  return gulp.src("src/files/**/*.*").pipe(gulp.dest("dist/files/"));
+};
+
+const html = () => {
+  return gulp
+    .src("src/*.html")
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream());
+};
+
+const sass = gulpSass(dartSass);
+
+const styles = () => {
+  return gulp
+    .src("src/scss/style.scss", { sourcemaps: true })
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      })
+    )
+    .pipe(gulp.dest("dist/css/"))
+    .pipe(browserSync.stream());
+};
+
+const server = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: "dist",
+    },
+    port: 3000,
+  });
+};
+
 function watcher() {
-  gulp.watch(path.watch.files, copy);
-  gulp.watch(path.watch.html, html);
+  gulp.watch("src/files/**/*.*", copy);
+  gulp.watch("src/**/*/*.html", html);
+  gulp.watch("src/scss/**/*.scss", styles);
 }
-const mainTasks = gulp.parallel(copy, html);
+
+const mainTasks = gulp.parallel(copy, html, styles);
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 gulp.task("default", dev);
