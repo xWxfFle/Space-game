fbd989d00c72d1cae4839ec6ccf4552080149fe9
xWxfFle <factores175@gmail.com>
2022-09-21 01:11:10 +0300

    added music

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,19 +1,45 @@
+const track = document.getElementById("track") as HTMLAudioElement;
+const volume = document.getElementById("volume") as HTMLInputElement;
+
+volume.addEventListener("change", () => {
+  track.volume = parseInt(volume.value) / 100;
+});
+
+
 import crashWith from "./modules/controllers/crashController.js";
 import ComponentFactory from "./modules/components/ComponentFactory.js";
 import Renderer from "./modules/components/ComponentRenderer.js";
 
-const canvas = document.getElementById("game-layer") as HTMLCanvasElement;
+const startbtn = document.getElementById("start") as HTMLButtonElement;
+const canvas = document.getElementById("game__layer") as HTMLCanvasElement;
 const renderer = new Renderer(canvas);
 const factory = new ComponentFactory(canvas);
 
+let isGameActive = false;
+let isGamePaused = false;
+startbtn.addEventListener("click", () => {
+  if (!isGameActive) {
+    startGame();
+    return;
+  }
+  isGamePaused = !isGamePaused;
+});
+
 function startGame() {
+  isGameActive = true;
+
   let frameNo = 0;
-  const obstacles: Array<any> = [];
+  let speedModificator = 0;
+  let spawnModificator = 0;
+
   const player = factory.createPlayer();
   const scoreBar = factory.createText(210, 50);
-
-  const healthContainer = ["🤍", "🤍", "🤍"];
+  const pauseBar = factory.createText(120, 220);
   const healthBar = factory.createText(30, 50);
+
+  const obstacles: Array<any> = [];
+  const healthContainer = ["🤍", "🤍", "🤍"];
+
   const directions = {
     ArrowLeft: false,
     ArrowUp: false,
@@ -21,87 +47,89 @@ function startGame() {
     ArrowRight: false,
   };
 
-  window.addEventListener("keydown", (e) => {
-    movementController(e.key, true);
+  window.addEventListener("keydown", (event) => {
+    movementController(event.key, true);
   });
-  window.addEventListener("mousedown", (e) => {
-    const clickedButton = (e.target as Element).closest("button");
+  window.addEventListener("mousedown", (event) => {
+    const clickedButton = (event.target as Element).closest("button");
     movementController(clickedButton?.className, true);
   });
-  window.addEventListener("touchstart", (e) => {
-    const clickedButton = (e.target as Element).closest("button");
+  window.addEventListener("touchstart", (event) => {
+    const clickedButton = (event.target as Element).closest("button");
     movementController(clickedButton?.className, true);
   });
 
-  window.addEventListener("keyup", (e) => {
-    movementController(e.key, false);
+  window.addEventListener("keyup", (event) => {
+    movementController(event.key, false);
   });
-  window.addEventListener("mouseup", (e) => {
-    const clickedButton = (e.target as Element).closest("button");
+  window.addEventListener("mouseup", (event) => {
+    const clickedButton = (event.target as Element).closest("button");
     movementController(clickedButton?.className, false);
   });
-  window.addEventListener("touchend", (e) => {
-    const clickedButton = (e.target as Element).closest("button");
+  window.addEventListener("touchend", (event) => {
+    const clickedButton = (event.target as Element).closest("button");
     movementController(clickedButton?.className, false);
   });
 
   const interval = setInterval(updateGameArea, 10);
 
-  let speedModificator = 0;
-  let spawnModificator = 0;
-
   function updateGameArea() {
     //Wiping canvas
     canvas.getContext("2d")?.clearRect(0, 0, canvas.width, canvas.height);
-    frameNo += 1;
 
-    obstacles.forEach((element: any, index) => {
-      if (element.y > canvas.height) return obstacles.splice(index, 1);
+    if (isGamePaused) {
+      pauseBar.text = "Game paused";
+      renderer.draw(pauseBar);
+    } else {
+      frameNo += 1;
+      obstacles.forEach((element: any, index) => {
+        if (element.y > canvas.height) return obstacles.splice(index, 1);
 
-      if (crashWith(player, element)) {
-        obstacles.splice(index, 1);
-        if (player.shield) return;
-        if (element.type === "shield") return player.activateShield;
-        if (healthContainer.length) return healthContainer.pop();
-        clearInterval(interval);
+        if (crashWith(player, element)) {
+          obstacles.splice(index, 1);
+          if (player.shield) return;
+          if (element.type === "shield") return player.activateShield;
+          if (healthContainer.length) return healthContainer.pop();
+          isGameActive = false;
+          clearInterval(interval);
+        }
+        element.y += 1 + speedModificator;
+      });
+
+      if ((frameNo / (120 - spawnModificator)) % 1 == 0) {
+        speedModificator < 3 ? (speedModificator += 0.1) : "";
+        spawnModificator < 100 ? (spawnModificator += 2) : "";
+        obstacles.push(factory.creacteAsteroid());
+      }
+      if ((frameNo / 1000) % 1 == 0) {
+        obstacles.push(factory.createShield());
       }
 
-      element.y += 1 + speedModificator;
-    });
+      if (directions.ArrowLeft) {
+        player.speedX = -4;
+      }
+      if (directions.ArrowRight) {
+        player.speedX = 4;
+      }
+      if (directions.ArrowUp) {
+        player.speedY = -4;
+      }
+      if (directions.ArrowDown) {
+        player.speedY = 4;
+      }
 
-    if ((frameNo / (120 - spawnModificator)) % 1 == 0) {
-      speedModificator < 3 ? (speedModificator += 0.1) : "";
-      spawnModificator < 100 ? (spawnModificator += 2) : "";
-      obstacles.push(factory.creacteAsteroid());
-    }
-    if ((frameNo / 1000) % 1 == 0) {
-      obstacles.push(factory.createShield());
-    }
+      //rendering all components
+      renderer.draw(obstacles);
 
-    if (directions.ArrowLeft) {
-      player.speedX = -4;
-    }
-    if (directions.ArrowRight) {
-      player.speedX = 4;
-    }
-    if (directions.ArrowUp) {
-      player.speedY = -4;
-    }
-    if (directions.ArrowDown) {
-      player.speedY = 4;
-    }
+      scoreBar.text = "SCORE: " + frameNo;
+      renderer.draw(scoreBar);
 
-    //rendering all components
-    renderer.draw(obstacles);
+      healthBar.text = healthContainer.join(" ");
+      renderer.draw(healthBar);
 
-    scoreBar.text = "SCORE: " + frameNo;
-    renderer.draw(scoreBar);
-
-    healthBar.text = healthContainer.join(" ");
-    renderer.draw(healthBar);
-
-    player.newPosition;
-    renderer.draw(player);
+      player.newPosition;
+      renderer.draw(player);
+    }
   }
 
   function movementController(listener: string | undefined, value: boolean) {
@@ -123,4 +151,3 @@ function startGame() {
     }
   }
 }
-startGame();
